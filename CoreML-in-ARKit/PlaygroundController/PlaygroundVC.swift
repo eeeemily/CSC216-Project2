52a66ab6d68452de152b0efd68130bd37ad41022
Minghui Zheng <mzheng9@u.rochester.edu>
2021-11-30 00:51:16 -0500

    added UI for user to self evaluate

--- CoreML-in-ARKit/PlaygroundController/PlaygroundVC.swift
+++ CoreML-in-ARKit/PlaygroundController/PlaygroundVC.swift
@@ -11,10 +11,30 @@ import RealityKit
 var instructorAnchor: Instructor.Person?
 class PlaygroundVC: UIViewController {
     @IBOutlet weak var arView: ARView!
+    @IBOutlet weak var EvalLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var evalYesBtn: UIButton!
+    @IBOutlet weak var evalNoBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        EvalLabel.isHidden = true
+        scoreLabel.isHidden = true
+        evalYesBtn.isHidden = true
+        evalNoBtn.isHidden = true
         instructorAnchor = try! Instructor.loadPerson()
         arView.scene.anchors.append(instructorAnchor!)
-        
+        instructorAnchor?.actions.clickJacketToShowASL.onAction = handleTapOnJacket(_:)
     }
+    
+    func handleTapOnJacket(_ entity: Entity?) {
+            guard let entity = entity else { return }
+            // Do something with entity...
+        EvalLabel.isHidden = false
+        EvalLabel.text = "Did you do the Jacket ASL sign correctly?"
+        scoreLabel.isHidden = false
+        evalYesBtn.isHidden = false
+        evalNoBtn.isHidden = false
+//        print("jacket is clicked")
+    }
+    
 }
